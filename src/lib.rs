a76eacd330edf001678ff63a874567f8e9f50441
Yusuke Nojima <nojima@ynojima.com>
2022-01-10 23:25:53 +0900

    Parser<T> を定義

--- src/lib.rs
+++ src/lib.rs
@@ -1,3 +1,16 @@
+// Parser<T> を Fn(&str) -> Option<(T, &str)> の別名(のようなもの)として定義する。
+pub trait Parser<T>: Fn(&str) -> Option<(T, &str)> {}
+impl<T, F> Parser<T> for F where F: Fn(&str) -> Option<(T, &str)> {}
+
+// クロージャの型推論を補助するための関数
+// cf. https://github.com/rust-lang/rust/issues/70263#issuecomment-623169045
+fn generalize_lifetime<T, F>(f: F) -> F
+where
+    F: Fn(&str) -> Option<(T, &str)>,
+{
+    f
+}
+
 // s の先頭にある整数をパースし、整数値と残りの文字列を返す。
 // パースに失敗した場合は None を返す。
 pub fn digits(s: &str) -> Option<(i64, &str)> {
@@ -17,15 +30,15 @@ fn test_digits() {
 }
 
 // 先頭の一文字が c であるときに成功して () を返すようなパーサーを返す。
-pub fn character(c: char) -> impl Fn(&str) -> Option<((), &str)> {
-    move |s| {
+pub fn character(c: char) -> impl Parser<()> {
+    generalize_lifetime(move |s| {
         let mut chars = s.chars();
         if chars.next() == Some(c) {
             Some(((), chars.as_str()))
         } else {
             None
         }
-    }
+    })
 }
 
 #[test]
